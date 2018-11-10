1b3c964a4e7d16a8612c0cc5f6b87432dc3d5e15
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2018-11-10 20:48:38 +0530

    important js file which will render to the front

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Authen from './Authen';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Authen/>
+      </div>
+    );
+  }
+}
+
+export default App;
